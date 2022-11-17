87c9e4e68649a95d237b98c1345108725227d386
=k-firoda <=>
2022-11-17 20:53:13 +0530

    first commit

---
+++ currency-conversion/77-higherOrderFunctions-start/src/js/index.js
@@ -0,0 +1,4 @@
+import cart from './cart.json';
+import currencyRates from './currencyRates.json';
+
+const currencyPicker = document.querySelector("select[name='currency-picker']");
---
+++ quotationMaster/src/js/quotes.js
@@ -0,0 +1,34 @@
+export const quotes = [
+  `Spread love everywhere you go.
+Let no one ever come to you without
+leaving happier.
+
+- Mother Teresa`,
+  `Always remember that you are 
+absolutely unique.
+Just like everyone else...
+
+- Margaret Mead`,
+  `Whoever is happy
+will make others happy too.
+
+- Anne Frank`,
+
+` The greatest glory in living lies 
+not in never falling, but in rising
+ every time we fall. 
+ 
+ 
+ -Nelson Mandela`,
+ ` The way to get started is to
+  quit talking and begin doing. 
+ 
+ 
+ -Walt Disney`,
+
+ ` If you set your goals ridiculously high
+  and it's a failure, you will fail
+   above everyone else's success. 
+   
+   -James Cameron`
+];
